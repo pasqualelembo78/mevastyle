0acac355a7cee4f38f5e677de3899796796dd6c2
pasqualelembo78 <55604527+pasqualelembo78@users.noreply.github.com>
2025-07-18 18:41:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ PORT=3050
 echo "==== AGGIORNAMENTO SISTEMA E INSTALLAZIONE DIPENDENZE ===="
 apt update && apt upgrade -y
 apt install -y git curl build-essential nodejs npm apache2 libxml2-dev
+sudo apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev pkg-config
+
 
 
 echo "==== INSTALLAZIONE YARN GLOBAL ===="
